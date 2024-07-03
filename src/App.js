12425c6cf10b9824be467786e67daf7a540808bc
Zaiin27 <zaintanveer271@gmail.com>
2024-07-03 05:13:53 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Welcome to React</h1>
+        <button href="/" >Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
